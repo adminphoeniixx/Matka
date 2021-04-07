78e70011ca6bcb6e90d2a0116231d3931a0cc779
Pratik Vaidya <prateek@phoeniixx.com>
2021-04-07 14:54:32 +0530

    HTTPS Force

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,7 @@ use App\Http\Controllers\NotificationsController;
 
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -80,3 +71,7 @@ Route::group(['prefix' => 'admin'], function () {
      Route::get('destroypushnotification/{id}', [NotificationsController::class, 'destroypushnotification'])->name('destroypushnotification');
 
 });
+
+
+
+URL::forceScheme('https');

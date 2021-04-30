3e30356df29e62a2dd85df64391212c730ff962f
vaidyasunny <83389129+vaidyasunny@users.noreply.github.com>
2021-04-30 16:19:24 +0530

    My-matches api

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -232,7 +232,7 @@ class UserController extends Controller
         ->leftJoin('winners','winners.betting_id','bettings.id')
         ->leftJoin('game_status','game_status.id','live_games.status')
         ->where('bettings.user',$request->user_id)
-        ->select('companies.name as company_name','companies.image','game_types.name as game_type','bettings.created_at','winners.amount as winning_amount','game_status.name as game_status')
+        ->select('companies.name as company_name','companies.image','game_types.name as game_type','bettings.created_at','winners.amount as winning_amount','game_status.name as game_status','bettings.id as betting_id')
         ->groupBy('bettings.live_game_id')
         ->get();
 

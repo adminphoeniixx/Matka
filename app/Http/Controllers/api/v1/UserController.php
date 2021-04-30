f4855b168c848bb8be5beee712955f383db9b745
vaidyasunny <83389129+vaidyasunny@users.noreply.github.com>
2021-04-30 17:07:07 +0530

    My-matches api

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -272,9 +272,10 @@ class UserController extends Controller
         ->select('companies.name as company_name','companies.image','game_types.name as game_type','bettings.created_at','winners.amount as winning_amount','game_status.name as game_status','bettings.id as betting_id','live_games.id as live_game_id')
         ->first();
 
+dd($betting);
         
 
-        $numbers = Betting::
+       // $numbers = Betting::
 
 
 
